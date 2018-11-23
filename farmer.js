bd3f577629c636117cec0903b6a33ec069fdf7b1
Pavel Djundik <xPaw@users.noreply.github.com>
2018-11-23 23:44:27 +0200

    Remove pics cache and ownership checking

--- farmer.js
+++ farmer.js
@@ -6,7 +6,7 @@ var prompt = require('prompt');
 var request = require('request');
 var Cheerio = require('cheerio');
 
-var client = new SteamUser({"enablePicsCache": true});
+var client = new SteamUser();
 
 var g_Jar = request.jar();
 request = request.defaults({"jar": g_Jar});
@@ -67,11 +67,6 @@ if(process.argv.length == argsStartIdx + 2) {
 
 client.on('loggedOn', function() {
 	log("Logged into Steam!");
-	log("Waiting for license info...");
-});
-
-client.once('appOwnershipCached', function() {
-	log("Got app ownership info");
 	checkMinPlaytime();
 });
 
@@ -96,14 +91,6 @@ function checkMinPlaytime() {
 			}
 			
 			var lowHourApps = [];
-			var ownedPackages = client.licenses.map(function(license) {
-				var pkg = client.picsCache.packages[license.package_id].packageinfo;
-				pkg.time_created = license.time_created;
-				pkg.payment_method = license.payment_method;
-				return pkg;
-			}).filter(function(pkg) {
-				return !(pkg.extended && pkg.extended.freeweekend);
-			});
 			
 			var $ = Cheerio.load(body);
 			$('.badge_row').each(function() {
@@ -124,23 +111,6 @@ function checkMinPlaytime() {
 				name.find('.badge_view_details').remove();
 				name = name.text().replace(/\n/g, '').replace(/\r/g, '').replace(/\t/g, '').trim();
 
-				// Check if app is owned
-				if(!client.ownsApp(appid)) {
-					log("Skipping app " + appid + " \"" + name + "\", not owned");
-					return;
-				}
-
-				var newlyPurchased = false;
-				// Find the package(s) in which we own this app
-				ownedPackages.filter(function(pkg) {
-					return pkg.appids && pkg.appids.indexOf(appid) != -1;
-				}).forEach(function(pkg) {
-					var timeCreatedAgo = Math.floor(Date.now() / 1000) - pkg.time_created;
-					if(timeCreatedAgo < (60 * 60 * 24 * 14) && [Steam.EPaymentMethod.ActivationCode, Steam.EPaymentMethod.GuestPass, Steam.EPaymentMethod.Complimentary].indexOf(pkg.payment_method) == -1) {
-						newlyPurchased = true;
-					}
-				});
-				
 				// Find out if we have drops left
 				var drops = row.find('.progress_info_bold').text().match(/(\d+)/);
 				if(!drops) {
@@ -170,106 +140,35 @@ function checkMinPlaytime() {
 						"appid": appid,
 						"name": name,
 						"playtime": playtime,
-						"newlyPurchased": newlyPurchased
 					});
-				}
-				
-				if(playtime >= 2.0 || !newlyPurchased) {
+				} else {
 					g_OwnedApps.push(appid);
 				}
 			});
 			
 			if(lowHourApps.length > 0) {
 				var minPlaytime = 2.0;
-				var newApps = [];
 				
 				lowHourApps.forEach(function(app) {
 					if(app.playtime < minPlaytime) {
 						minPlaytime = app.playtime;
 					}
-					
-					if(app.newlyPurchased) {
-						newApps.push(app);
-					}
+
+					log('App ' + app.appid + ' - ' + app.name + ' - Playtime: ' + app.playtime);
 				});
 				
-				var lowAppsToIdle = [];
-				
-				if(newApps.length > 0) {
-					log("=========================================================");
-					log("WARNING: Proceeding will waive your right to a refund on\nthe following apps:\n  - " + newApps.map(function(app) { return app.name; }).join("\n  - ") +
-						"\n\nDo you wish to continue?\n" +
-						"    y = yes, idle all of these apps and lose my refund\n" +
-						"    n = no, don't idle any of these apps and keep my refund\n" +
-						"    c = choose which apps to idle");
-					
-					prompt.start();
-					prompt.get({
-						"properties": {
-							"choice": {
-								"required": true,
-								"pattern": /^[yncYNC]$/
-							}
-						}
-					}, function(err, result) {
-						if(err) {
-							log("ERROR: " + err.message);
-							return;
-						}
-						
-						switch(result.choice.toLowerCase()) {
-							case 'y':
-								lowAppsToIdle = lowHourApps.map(function(app) { return app.appid; });
-								startErUp();
-								break;
-							
-							case 'n':
-								lowAppsToIdle = [];
-								startErUp();
-								break;
-							
-							case 'c':
-								var properties = {};
-								lowHourApps.forEach(function(app) {
-									properties[app.appid] = {
-										"description": "Idle " + app.name + "? [y/n]",
-										"pattern": /^[ynYN]$/,
-										"required": true
-									};
-								});
-								
-								prompt.get({"properties": properties}, function(err, result) {
-									for(var appid in result) {
-										if(isNaN(parseInt(appid, 10))) {
-											continue;
-										}
-										
-										if(result[appid].toLowerCase() == 'y') {
-											lowAppsToIdle.push(parseInt(appid, 10));
-										}
-									}
-									
-									startErUp();
-								});
-						}
-					});
+				var lowAppsToIdle = lowHourApps.map(function(app) { return app.appid; });
+
+				if(lowAppsToIdle.length < 1) {
+					checkCardApps();
 				} else {
-					lowAppsToIdle = lowHourApps.map(function(app) { return app.appid; });
-					startErUp();
-				}
-				
-				function startErUp() {
-					if(lowAppsToIdle.length < 1) {
+					g_OwnedApps = g_OwnedApps.concat(lowAppsToIdle);
+					client.gamesPlayed(lowAppsToIdle);
+					log("Idling " + lowAppsToIdle.length + " app" + (lowAppsToIdle.length == 1 ? '' : 's') + " up to 2 hours.\nYou likely won't receive any card drops in this time.\nThis will take " + (2.0 - minPlaytime) + " hours.");
+					setTimeout(function() {
+						client.gamesPlayed([]);
 						checkCardApps();
-					} else {
-						g_OwnedApps = g_OwnedApps.concat(lowAppsToIdle);
-						client.gamesPlayed(lowAppsToIdle);
-						log("Idling " + lowAppsToIdle.length + " app" + (lowAppsToIdle.length == 1 ? '' : 's') + " up to 2 hours.\nYou likely won't receive any card drops in this time.\nThis will take " + (2.0 - minPlaytime) + " hours.");
-						setTimeout(function() {
-							client.gamesPlayed([]);
-							checkCardApps();
-						}, (1000 * 60 * 60 * (2.0 - minPlaytime)));
-					}
+					}, (1000 * 60 * 60 * (2.0 - minPlaytime)));
 				}
 			} else {
 				checkCardApps();
@@ -382,3 +281,4 @@ function shutdown(code) {
 		process.exit(code);
 	}, 500);
 }
+
