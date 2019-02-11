34db2c95701626701997dc352ca2a942663a9f0d
Pavel Djundik <xPaw@users.noreply.github.com>
2019-02-11 14:43:54 +0200

    Move webSession handler and don't request a session on logon

--- farmer.js
+++ farmer.js
@@ -61,119 +61,9 @@ function checkCardsInSeconds(seconds, callback) {
 }
 
 function checkCardApps() {
-	log('Checking card drops...');
+	log('Requesting a web session...');
 
 	client.webLogOn();
-	client.once('webSession', (sessionID, cookies) => {
-		cookies.forEach((cookie) => {
-			g_Jar.setCookie(cookie, 'https://steamcommunity.com');
-		});
-
-		request({
-			url: `https://steamcommunity.com/my/badges/?l=english&p=${g_Page}`,
-			maxRedirects: 1,
-			timeout: 30000,
-		}, (err, response, body) => {
-			if (err || response.statusCode !== 200) {
-				log(`Couldn't request badge page: ${err || `HTTP error ${response.statusCode}`}`);
-				checkCardsInSeconds(30);
-				return;
-			}
-
-			if (body.includes('g_steamID = false')) {
-				log('Badge page loaded, but its logged out');
-				checkCardsInSeconds(30);
-				return;
-			}
-
-			let lowHourApps = [];
-			const hasDropsApps = [];
-
-			const $ = Cheerio.load(body);
-
-			$('.progress_info_bold').each((index, infoline) => {
-				const match = $(infoline).text().match(/(\d+)/);
-				const row = $(infoline).closest('.badge_row');
-				const href = row.find('.badge_title_playgame a').attr('href');
-
-				if (!match || !href) {
-					return;
-				}
-
-				const urlparts = href.split('/');
-				const appid = parseInt(urlparts[urlparts.length - 1], 10) || 0;
-				const drops = parseInt(match[1], 10) || 0;
-
-				if (appid < 1 || drops < 1) {
-					return;
-				}
-
-				let title = row.find('.badge_title');
-				title.find('.badge_view_details').remove();
-				title = title.text().trim();
-
-				let playtime = parseFloat(row.find('.badge_title_stats').html().match(/(\d+\.\d+)/), 10) || 0.0;
-				playtime = Math.round(playtime * 60);
-
-				const appObj = {
-					appid,
-					title,
-					playtime,
-					drops,
-				};
-
-				if (playtime < 120) {
-					lowHourApps.push(appObj);
-				} else {
-					hasDropsApps.push(appObj);
-				}
-			});
-
-			if (lowHourApps.length > 0) {
-				let minPlaytime = 120;
-
-				lowHourApps = lowHourApps.slice(0, 32);
-				lowHourApps.forEach((app) => {
-					if (app.playtime < minPlaytime) {
-						minPlaytime = app.playtime;
-					}
-
-					log(`App ${app.appid} - ${app.title} - Playtime: ${app.playtime} min`);
-				});
-
-				log(`Idling ${lowHourApps.length} app${lowHourApps.length === 1 ? '' : 's'} up to 2 hours.`);
-				log(`This will take ${minPlaytime} minutes.`);
-
-				client.gamesPlayed(lowHourApps.map(app => app.appid));
-
-				const delay = 60 * (120 - minPlaytime);
-				checkCardsInSeconds(delay, () => {
-					log('Stopped idling previous apps');
-					client.gamesPlayed([]);
-				});
-			} else if (hasDropsApps.length > 0) {
-				const totalDropsLeft = hasDropsApps.reduce((sum, { drops }) => sum + drops, 0);
-				const appToIdle = hasDropsApps[0];
-
-				log(`${totalDropsLeft} card drop${totalDropsLeft === 1 ? '' : 's'} remaining across ${hasDropsApps.length} app${hasDropsApps.length === 1 ? '' : 's'} (Page ${g_Page})`);
-				log(`Idling app ${appToIdle.appid} "${appToIdle.title}" - ${appToIdle.drops} drop${appToIdle.drops === 1 ? '' : 's'} remaining`);
-
-				client.gamesPlayed(appToIdle.appid);
-
-				// 20 minutes to be safe, we should automatically check when
-				// Steam notifies us that we got a new item anyway
-				checkCardsInSeconds(1200);
-			} else if (g_Page <= (parseInt($('.pagelink').last().text(), 10) || 1)) {
-				log(`No drops remaining on page ${g_Page}`);
-				g_Page += 1;
-				log(`Checking page ${g_Page}`);
-				checkCardsInSeconds(1);
-			} else {
-				log('All card drops recieved! Shutting down...');
-				shutdown(0);
-			}
-		});
-	});
 }
 
 let argsStartIdx = 2;
@@ -226,7 +116,6 @@ process.on('SIGINT', () => {
 
 client.on('loggedOn', () => {
 	log('Logged into Steam!');
-	checkCardApps();
 });
 
 client.on('error', (e) => {
@@ -278,4 +167,117 @@ client.on('playingState', (blocked, appid) => {
 	if (g_CheckTimer) {
 		clearTimeout(g_CheckTimer);
 	}
+});
+
+client.on('webSession', (sessionID, cookies) => {
+	log('Got a web session, checking card drops...');
+
+	cookies.forEach((cookie) => {
+		g_Jar.setCookie(cookie, 'https://steamcommunity.com');
+	});
+
+	request({
+		url: `https://steamcommunity.com/my/badges/?l=english&p=${g_Page}`,
+		maxRedirects: 1,
+		timeout: 30000,
+	}, (err, response, body) => {
+		if (err || response.statusCode !== 200) {
+			log(`Couldn't request badge page: ${err || `HTTP error ${response.statusCode}`}`);
+			checkCardsInSeconds(30);
+			return;
+		}
+
+		if (body.includes('g_steamID = false')) {
+			log('Badge page loaded, but its logged out');
+			checkCardsInSeconds(30);
+			return;
+		}
+
+		let lowHourApps = [];
+		const hasDropsApps = [];
+
+		const $ = Cheerio.load(body);
+
+		$('.progress_info_bold').each((index, infoline) => {
+			const match = $(infoline).text().match(/(\d+)/);
+			const row = $(infoline).closest('.badge_row');
+			const href = row.find('.badge_title_playgame a').attr('href');
+
+			if (!match || !href) {
+				return;
+			}
+
+			const urlparts = href.split('/');
+			const appid = parseInt(urlparts[urlparts.length - 1], 10) || 0;
+			const drops = parseInt(match[1], 10) || 0;
+
+			if (appid < 1 || drops < 1) {
+				return;
+			}
+
+			let title = row.find('.badge_title');
+			title.find('.badge_view_details').remove();
+			title = title.text().trim();
+
+			let playtime = parseFloat(row.find('.badge_title_stats').html().match(/(\d+\.\d+)/), 10) || 0.0;
+			playtime = Math.round(playtime * 60);
+
+			const appObj = {
+				appid,
+				title,
+				playtime,
+				drops,
+			};
+
+			if (playtime < 120) {
+				lowHourApps.push(appObj);
+			} else {
+				hasDropsApps.push(appObj);
+			}
+		});
+
+		if (lowHourApps.length > 0) {
+			let minPlaytime = 120;
+
+			lowHourApps = lowHourApps.slice(0, 32);
+			lowHourApps.forEach((app) => {
+				if (app.playtime < minPlaytime) {
+					minPlaytime = app.playtime;
+				}
+
+				log(`App ${app.appid} - ${app.title} - Playtime: ${app.playtime} min`);
+			});
+
+			log(`Idling ${lowHourApps.length} app${lowHourApps.length === 1 ? '' : 's'} up to 2 hours.`);
+			log(`This will take ${minPlaytime} minutes.`);
+
+			client.gamesPlayed(lowHourApps.map(app => app.appid));
+
+			const delay = 60 * (120 - minPlaytime);
+			checkCardsInSeconds(delay, () => {
+				log('Stopped idling previous apps');
+				client.gamesPlayed([]);
+			});
+		} else if (hasDropsApps.length > 0) {
+			const totalDropsLeft = hasDropsApps.reduce((sum, { drops }) => sum + drops, 0);
+			const appToIdle = hasDropsApps[0];
+
+			log(`${totalDropsLeft} card drop${totalDropsLeft === 1 ? '' : 's'} remaining across ${hasDropsApps.length} app${hasDropsApps.length === 1 ? '' : 's'} (Page ${g_Page})`);
+			log(`Idling app ${appToIdle.appid} "${appToIdle.title}" - ${appToIdle.drops} drop${appToIdle.drops === 1 ? '' : 's'} remaining`);
+
+			client.gamesPlayed(appToIdle.appid);
+
+			// 20 minutes to be safe, we should automatically check when
+			// Steam notifies us that we got a new item anyway
+			checkCardsInSeconds(1200);
+		} else if (g_Page <= (parseInt($('.pagelink').last().text(), 10) || 1)) {
+			log(`No drops remaining on page ${g_Page}`);
+			g_Page += 1;
+			log(`Checking page ${g_Page}`);
+			checkCardsInSeconds(1);
+		} else {
+			log('All card drops received! Shutting down...');
+			shutdown(0);
+		}
+	});
 });
