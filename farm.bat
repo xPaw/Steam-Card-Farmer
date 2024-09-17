467c064032c0fb27838d050737e10fe495266ca8
Pavel Djundik <xPaw@users.noreply.github.com>
2024-09-17 20:05:26 +0300

    Check if node is installed and packages installed

--- farm.bat
+++ farm.bat
@@ -1,3 +1,19 @@
 @echo off
+
+where node >nul 2>nul
+if %errorlevel% neq 0 (
+	echo Node.js is not installed.
+	echo Please install Node.js from https://nodejs.org/
+	echo or run "winget install OpenJS.NodeJS" command
+	pause
+	exit /b 1
+)
+
+if not exist "%~dp0node_modules" (
+	echo node_modules folder not found, please run \"npm install --omit=dev\"
+	pause
+	exit /b 1
+)
+
 node %~dp0index.mjs %*
 pause
